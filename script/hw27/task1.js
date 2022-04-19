1c107d5a7f1cf3ccd2e72b846e65d84dd455bedf
dronovs <od270891dss@gmail.com>
2022-04-19 16:46:42 +0300

    update

--- script/hw27/task1.js
+++ script/hw27/task1.js
@@ -1,2 +1,30 @@
 'use strict'
 
+function getPalindrome (num) {
+    let steps = 0;
+
+    function getReverseNumber (a) {
+        return parseInt(a.toString().split('').reverse().join(''));
+    }
+
+    function sumUntilPalindrome (num) {
+
+        if (num === getReverseNumber(num)) {
+            return {
+                num,
+                steps: steps,
+            };
+        } else {
+            steps++;
+            num += getReverseNumber(num);
+            return sumUntilPalindrome(num);
+        }
+
+    }
+    
+    return sumUntilPalindrome(num);
+}
+
+const result = getPalindrome(96);
+console.log(result);
+
